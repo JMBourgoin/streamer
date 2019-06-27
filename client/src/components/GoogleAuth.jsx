99c5ad3a9e808ef052411c0326f3bb7e5f5a4449
Jason Bourgoin <jason@jmbourgoin.com>
2019-06-27 17:00:37 -0400

    add login / logout buttons

--- client/src/components/GoogleAuth.jsx
+++ client/src/components/GoogleAuth.jsx
@@ -10,7 +10,7 @@ class GoogleAuth extends React.Component {
     }
 
     componentDidMount(){
-        window.gapi.load('client:auth2', ()=>{
+        window.gapi.load('client:auth2', () => {
             window.gapi.client.init({
                 clientId: '13840900868-eribe0vh4io28n1ebfo1v82gbh0afkuu.apps.googleusercontent.com',
                 scope: 'email'
@@ -21,17 +21,41 @@ class GoogleAuth extends React.Component {
         });
     }
 
+   logOut = (e) => {
+        e.preventDefault();
+       this.auth.signOut();
+       this.setState({
+           isSignedIn: false
+       });
+    }
+
+   logIn = (e) => {
+        e.preventDefault();
+       this.auth.signIn().then(() => {
+           this.setState({
+            isSignedIn: true
+            });
+        });  
+    }
+
     renderAuthButton(){
         if(this.state.isSignedIn === null) {
-            return <div>I don't know if we are signed in</div>
+            return null;
         } else if (this.state.isSignedIn) {
-            return <div>I am signed in</div>
+            return <button className="ui red google button" onClick={this.logOut}>
+                    <i className="google icon"></i>
+                    log out
+                 </button>
         } else {
-            return <div>I am not signed in</div>
+            return <button className="ui red google button" onClick={this.logIn}>
+                    <i className="google icon"></i>
+                    log in
+                   </button>
         }
     }
 
     render(){
+      
         return (<div>{this.renderAuthButton()}</div>)
     }
 }
